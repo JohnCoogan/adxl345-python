a2e171d7aec7c832d9daa45ed4976bf120b57ec5
John Coogan <coogan.johna@gmail.com>
2014-09-23 22:30:48 -0700

    using subprocesses and a separate logger file

--- watcher.py
+++ watcher.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import os
 import sys
 import sheetsync
-import threading
+import subprocess
 
 try:  
     pw = os.environ["GSHEETS"]
@@ -33,9 +33,9 @@ print "   z = %.3fG" % ( axes['z'] )
 oldaxes = dict(axes)
 
 start_time = time.time()
-results = {'force': 0, 'bumps': 0}
+results = {'force': 0, 'bumps': 0, 'reading':'startup'}
 target = sheetsync.Sheet(username="coogan.johna@gmail.com", password=pw, document_name="SleepLog")
-
+target.inject({datetime.now():result_log})
 
 def log_results(result_log):
     target.inject({datetime.now():result_log})
@@ -47,11 +47,12 @@ def log_results(result_log):
 while True:
     diff_time = time.time() - start_time
     if diff_time >= 60.0:
+        results['reading'] = 'minute'
         print datetime.now()
         start_time = time.time()
         result_log = dict(results)
-        threading.Thread(target=log_results, args=(result_log,)).start()
-        log_results(result_log)
+        subprocess.Popen(["sudo -E python /home/pi/toaster/adxl345-python/logger.py",result_log['force'],result_log['bumps']])
+        # log_results(result_log)
         results = {'force': 0, 'bumps': 0}
     axes = adxl345.getAxes(True)
     deltas = {k:abs(v - oldaxes[k]) for k,v in axes.items()}
