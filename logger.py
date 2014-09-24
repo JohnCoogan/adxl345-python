ab143d37480c5fe7bf5f06a4d0350dfdfc52ffeb
John Coogan <coogan.johna@gmail.com>
2014-09-23 22:51:28 -0700

    trying mongodb logging

--- logger.py
+++ logger.py
@@ -13,8 +13,18 @@ local_str = local_date.strftime(date_format)
 password = os.environ["GSHEETS"]
 username = "coogan.johna@gmail.com"
 
-target = sheetsync.Sheet(username=username, password=password, document_name="SleepLog")
+import pymongo
+password = os.environ["GSHEETS"]
+mongo_uri = "mongodb://johncoogan:%s@lighthouse.1.mongolayer.com:10243,lighthouse.0.mongolayer.com:10243/testing" % password
+connect = pymongo.Connection(mongo_uri)
+mongo = connect.sleeplog
 
-results = {'force': float(sys.argv[1]), 'bumps': int(sys.argv[2])}
 
-target.inject({local_str:results})
+
+# target = sheetsync.Sheet(username=username, password=password, document_name="SleepLog")
+
+results = {'datetime': local_date, 'datestr': local_str, 'force': float(sys.argv[1]), 'bumps': int(sys.argv[2])}
+
+# target.inject({local_str:results})
+
+mongo.save(results)
